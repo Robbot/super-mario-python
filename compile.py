c4a0acbbb23163c0e169a4a860374e022f6b803b
Robbot <1743138+Robbot@users.noreply.github.com>
2024-09-15 20:52:33 +0100

    main window designed in PyQt6

--- compile.py
+++ compile.py
@@ -5,7 +5,7 @@ import glob
 
 setup(
     # this is the file that is run when you start the game from the command line.
-    console=["main.py"],
+    console=["student.py"],
     # data files - these are the non-python files, like images and sounds
     data_files=[
         ("sprites", glob.glob("sprites\\*.json")),
